23887e0e5109f7733b037b5452162bf1c2826120
cduck1 <64195678+cduck1@users.noreply.github.com>
2020-06-05 16:46:22 +0100

    Update collisions.py

--- PyGame/collisions.py
+++ PyGame/collisions.py
@@ -33,6 +33,7 @@ def gameloop():
     x_offset = -1
     y_offset = 1
     lives = 3
+    score = 0
     
     while not done:
         # -- User input and controls 
@@ -47,12 +48,13 @@ def gameloop():
                 y_padd = y_padd + 5
             
         # -- Game logic goes after this comment
+        # -- The paddle can't go off the top or bottom
         if y_padd <= 0:
             y_padd = 0
             
         if y_padd >= 420:
             y_padd = 420
-    
+
         # -- Getting the score to increase by 1 when the ball goes off the left edge
         if x_val == 0:
             lives = lives - 1
@@ -74,11 +76,15 @@ def gameloop():
             y_val =  460
             y_offset = -y_offset
                
-        # -- Check the criteria for hitting the paddle
+        # -- Check the criteria for hitting the paddle and getting the ball speed to increase with a score variable
         if x_val < 15 and y_val > y_padd and y_val < y_padd + 60:
             x_offset = x_offset * -1
-            x_val = x_val + x_offset 
-            y_val = y_val + y_offset
+            score = score + 1
+            x_offset += 1
+            y_offset += 1
+            
+        x_val = x_val + x_offset 
+        y_val = y_val + y_offset
             
         # -- Screen background is BLACK 
         screen.fill(BLACK)
@@ -87,9 +93,9 @@ def gameloop():
         pygame.draw.rect(screen, BLUE, (x_val, y_val, ball_width, ball_width)) 
         pygame.draw.rect(screen, WHITE, (x_padd, y_padd, padd_width, padd_length))
         # -- display lives
-        font = pygame.font.Font(None, 60)
-        text = font.render(str(lives), 1, WHITE)
-        screen.blit(text, (250, 10))
+        font = pygame.font.Font(None, 50)
+        text = font.render("Lives: " + str(lives), 1, WHITE)
+        screen.blit(text, (310, 10))
         
         # -- getting the game to end when lives = 0
         if lives == 0:
@@ -105,9 +111,9 @@ def gameloop():
 # -- getting the game to end when lives = 0
 def gameover():
     screen.fill(BLACK)
-    font = pygame.font.Font("freesansbold.ttf", 60)
+    font = pygame.font.Font("freesansbold.ttf", 50)
     text = font.render("GAME OVER", 1, WHITE)
-    screen.blit(text, (250, 10))
+    screen.blit(text, (310, 10))
     pygame.display.flip()
     clock.tick(60)
         
